2b1af2f16d9342032510b98b3af6ecdcf12de15a
MariMorse <marianne.morse@bryanuniversity.edu>
2022-04-05 22:45:05 -0400

    commit

--- Week1/todo/src/App.js
+++ Week1/todo/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <p>My todo list for today</p>
+            <input type="checkbox" />Get Coffee<br/>
+            
+            <input type="checkbox" />Morning Run<br/>
+            
+            <input type="checkbox" />Take Shower<br/>
+            
+            <input type="checkbox" />Start Schoolwork,Study<br/>
+        </div>
+    )
 }
 
-export default App;
+export default App
